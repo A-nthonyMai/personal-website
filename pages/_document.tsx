501aedd01e2941d1ac8d64931699807adf5b2774
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-05 01:05:00 +1100

    Changed back to h-screen

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-[#181a1b] h-full text-gray-300'>
+      <body className='bg-[#181a1b] h-screen text-gray-300'>
         <Main />
         <NextScript />
       </body>
