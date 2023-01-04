2915ccac1a549b9077187a888f2ec093e663e674
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-04 13:13:10 +1100

    Updated design of profile image and about me

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,21 +13,17 @@ export default function Home() {
         <meta name="description" content="Anthony Mai's Personal Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className={styles.main}>
-        <div>
-          <Image
-            className={styles.logo}
-            src="/images/profile.jpg"
-            alt="Photo of Anthony Mai"
-            width={400}
-            height={400}
-            priority
-          />
-        </div>
-        <section>
-          <p>
-            Hi, I&apos;m Anto and I&apos;m a software engineer based out of Melbourne, Australia.
-          </p>
+      <main className="flex items-center justify-center py-48">
+        <Image
+          className="rounded-full"
+          src="/images/anthony-mai.avif"
+          alt="Photo of Anthony Mai"
+          width={400}
+          height={400}
+        />
+        <section className='px-32'>
+          <p className="text-5xl my-8">Hi! I&apos;m <span className="text-mint-green font-semibold">Anto</span>.</p>
+          <p className="text-3xl my-8">Software Engineer at <Link className="hover:underline decoration-mint-green" href="https://www.slalombuild.com/">Slalom<span className="text-[#2fe4f3]">_build</span></Link></p>
         </section>
       </main>
     </>
