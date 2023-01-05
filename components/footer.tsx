5699c7b09aac759ad82af921942c7728ffcc6ace
Anthony Mai <Anthony.mai@hotmail.com>
2023-01-05 21:08:03 +1100

    Added sticky footer (#9)
    * Updated README
    
    * Added sticky footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+const footer = () => {
+    return (
+        <footer className="w-full sticky top-[100vh] h-8 font-normal text-center text-sm lg:text-lg">
+            View the source code <a className="text-mint-green hover:underline" href="https://github.com/A-nthonyMai/personal-website">here</a>.
+        </footer>
+    )
+}
+
+export default footer;
--- pages/home.tsx
+++ pages/home.tsx
@@ -7,7 +7,7 @@ const profileID = 'a-nthonymai'
 const Home = () => {
     return (
         <div>
-            <div className="ml-6 flex items-center justify-center py-4 md:py-24 md:px-24">
+            <div className="ml-6 flex items-center justify-center md:py-4 lg:py-12 lg:px-12">
                 <Image
                     className="w-36 h-36 md:w-48 md:h-48 lg:w-96 lg:h-96 rounded-full"
                     src="/images/anthony-mai.avif"
@@ -16,23 +16,23 @@ const Home = () => {
                     height={400}
                 />
                 <section className='ml-6 md:ml-16 lg:ml-32'>
-                    <p className="text-3xl my-2 md:text-8xl md:my-4 lg:my-8">Hi! I&apos;m <span className="text-mint-green font-semibold">Anto</span>.</p>
-                    <p className="text-1xl my-2 md:text-2xl md:my-4 lg:my-8">Software Engineer at <Link className="hover:underline decoration-mint-green" href="https://www.slalombuild.com/">Slalom<span className="text-[#2fe4f3]">_build</span></Link></p>
+                    <p className="text-3xl my-2 md:my-4 lg:my-8 lg:text-8xl">Hi! I&apos;m <span className="text-mint-green font-semibold">Anto</span>.</p>
+                    <p className="text-1xl my-2 md:my-4 lg:my-8 lg:text-2xl">Software Engineer at <Link className="hover:underline decoration-mint-green" href="https://www.slalombuild.com/">Slalom<span className="text-[#2fe4f3]">_build</span></Link></p>
 
                     <div className="inline-flex">
                         <IconButton
                             iconPath={'/images/linkedIn-icon.svg'}
-                            cssClasses="w-10 h-10 md:w-16 mr-7 md:h-16 invert"
+                            cssClasses="mr-7 w-10 h-10 lg:w-16 lg:h-16 invert"
                             link={`https://www.linkedin.com/in/${profileID}`}
                         />
                         <IconButton
                             iconPath={'/images/email-icon.svg'}
-                            cssClasses="w-10 h-10 md:w-16 mr-7 md:h-16 invert"
+                            cssClasses="mr-7 w-10 h-10 lg:w-16 lg:h-16 invert"
                             link={`mailto:anthony.mai@hotmail.com?subject=Hi Anto.au!`}
                         />
                         <IconButton
                             iconPath={'/images/github-icon.svg'}
-                            cssClasses="w-10 h-10 md:w-16 md:h-16"
+                            cssClasses="w-10 h-10 lg:w-16 lg:h-16"
                             link={`https://www.github.com/${profileID}`}
                         />
                     </div>
