7fbb1101363f21062407ecbb81c2408717a44764
Anthony Mai <Anthony.mai@hotmail.com>
2023-01-06 13:34:56 +1100

    Created navigation bar (#10)
    * Implemented nav bar
    
    * Updated margin and font weight

---
+++ components/navButton.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+
+type NavButtonProps = {
+    buttonText: string
+    link: string
+    borderColour?: string
+}
+
+const NavButton = ({ buttonText, link, borderColour }: NavButtonProps) => {
+    const buttonCSS = 'w-36 md:w-48 lg:w-64 sticky top-[100vh] h-12 lg:h-8 font-medium text-center text-lg lg:text-xl'
+    return (
+        <div>
+            <button className={borderColour ? `border-b-4 ${borderColour} ${buttonCSS}` : buttonCSS}>
+                <Link href={link}>
+                    {buttonText}
+                </Link>
+            </button>
+        </div>
+    )
+}
+
+export default NavButton
