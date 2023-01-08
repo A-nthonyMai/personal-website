0ff6a6985ff0e6e933bc2a8e84643eee38151647
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-09 10:42:47 +1100

    Added nav button widths for medium and large devices

--- components/navButton.tsx
+++ components/navButton.tsx
@@ -7,11 +7,11 @@ type NavButtonProps = {
 }
 
 const NavButton = ({ buttonText, link, borderColour }: NavButtonProps) => {
-    const navButtonCSS = 'w-1/3 text-center'
+    const navButtonCSS = 'w-1/3 md:w-48 lg:w-64 text-center'
 
     return (
         <div className={borderColour ? `border-b-4 ${borderColour} ${navButtonCSS}` : navButtonCSS}>
-            <button className='h-12 lg:h-8 font-medium text-lg lg:text-xl'>
+            <button className='h-12 lg:h-8 font-medium text-sm md:text-lg lg:text-xl'>
                 <Link href={link}>
                     {buttonText}
                 </Link>
