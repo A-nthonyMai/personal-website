d832941a08c07924ee8770df0d54c90ef3d69c70
Anthony Mai <Anthony.Mai@hotmail.com>
2023-01-17 11:42:20 +1100

    Fixed download file button to serve resume from public folder

--- components/sections/resume.tsx
+++ components/sections/resume.tsx
@@ -1,16 +1,19 @@
-
-
-const downloadFile = () => {
-    window.open('https://drive.google.com/u/1/uc?id=1BA5kWl8drLoLhycfRX76UzU4iQPqokAv&export=download', '_blank')
-}
 const Resume = () => {
     return (
-        <section id="resume" className="w-full sm:h-screen flex flex-col">
+        <section id="resume" className="w-full sm:min-h-screen flex flex-col">
             <h1 className="text-3xl mx-1 my-5 md:text-5xl lg:mx-7 lg:my-7">Resume</h1>
-            <div className="overflow-hidden flex flex-row">
-                <iframe className="w-screen h-screen mx-1 lg:mx-7 md:w-1/3" src="https://drive.google.com/file/d/1BA5kWl8drLoLhycfRX76UzU4iQPqokAv/preview#view=fitH`" />
-                <button className={`w-36 rounded-md h-9 border border-solid 
-                         hover:border-2 font-bold text-sm md:text-md text-center hover:border-starship-yellow`} onClick={() => downloadFile()}>Download File</button>
+            <div className="overflow-hidden flex flex-col sm:flex-row">
+                <iframe className="w-screen h-screen mx-1 lg:mx-7 md:w-4/5 lg:w-3/5 xl:w-2/5" src={`https://drive.google.com/file/d/1YqvlhIfvGME_BmJOMbLL7wLRliZ5mnYJfZFE1Ns2w5M/preview#view=fitH`} />
+                <a
+                    download
+                    href="/assets/Anthony Mai - Resume.pdf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={`w-36 rounded-md h-9 border border-solid 
+                         hover:border-2 font-bold text-sm md:text-md text-center hover:border-starship-yellow leading-loose`}
+                >
+                    Download File
+                </a>
             </div>
         </section>
     )
